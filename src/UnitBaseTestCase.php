fabb308f0a27f1444d3dd139904e3c8d108d17c5
houdunwang <2300071698@qq.com>
2017-04-23 23:49:59 +0800

    no message

---
+++ src/UnitBaseTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/** .-------------------------------------------------------------------
+ * |  Software: [HDPHP framework]
+ * |      Site: www.hdphp.com  www.hdcms.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+
+namespace houdunwang\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 单元测试基础类
+ * Class UnitBaseTestCase
+ *
+ * @package houdunwang\unit
+ */
+class UnitBaseTestCase extends TestCase
+{
+
+}
